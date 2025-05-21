7b701d070bd31ebb5df693293c2f956b463469e1
Aqua <notnotsky26@gmail.com>
2025-05-21 14:06:59 +0000

    fixed smth

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -8,6 +9,10 @@ const nextConfig: NextConfig = {
         hostname: "**",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@ui"] = path.resolve(__dirname, "src/components/ui");
+    return config;
   },
 };
 
