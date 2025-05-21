97881688d81e8f47570b7d1ce5942d8b7fa16fcf
Aqua <notnotsky26@gmail.com>
2025-05-21 13:46:34 +0000

    fixed import alias

--- src/app/articleList/page.tsx
+++ src/app/articleList/page.tsx
@@ -2,8 +2,8 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../../lib/supabaseClient";
 import { ArticleCard } from "@ui/articleCard";
-import Header from "@ui/header";
-import Footer from "@ui/footer";
+// import Header from "@ui/header";
+// import Footer from "@ui/footer";
 
 import Link from "next/link";
 
@@ -42,4 +42,26 @@ export default function ArticlesPage() {
       <Footer />
     </div>
   );
+}
+import DropdownMenu from "@ui/dropDownMenu";
+import { motion } from "framer-motion";
+import Hero from "@ui/hero";
+import Link from "next/link";
+ function Header() {
+  return (
+    <>
+      <div className="flex justify-start items-center m-1   w-auto h-[8vh] bg-white">
+        <Link href="/">
+          <h1 className="min-text-3xl text-5xl font-bold ">Nannuru</h1>
+        </Link>
+        <DropdownMenu className="ml-auto bg-gray-000" />
+      </div>
+    </>
+  );
+}
+ function Footer() {
+  return <div className="mt-24"></div>;
+}
+function Footer() {
+  return <div className="mt-24"></div>;
 }
